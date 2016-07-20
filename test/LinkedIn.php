76e4245b1f6374b87e98fd9fd88c6b838f04637c
shawntharoo <mr.stadikaram@gmail.com>
2016-07-20 14:43:31 +0530

    LinkedIn Edit

--- test/LinkedIn.php
+++ test/LinkedIn.php
@@ -1,11 +1,12 @@
 <html>
 	<head>
+		<title>LinkedIn Authenticatio</title>
 		<?php
 			session_start();
 			$_SESSION["memberid"] = $_GET['memberid'];
 		?>
     <font size="5"; color="black">Signing with your LinkedIn Account </font><br/><br/>
-    <font size="3"; color="black">Click the Button Below </font><br/><br/>
+    <font size="4"; color="black">Click the Button Below </font><br/><br/>
 		<script type="text/javascript" src="http://platform.linkedin.com/in.js">
 			api_key: 75zaxqxnhgxdet
 			onLoad: OnLinkedInFrameworkLoad
@@ -22,17 +23,25 @@
 		}
 
 		function OnLinkedInAuth() {
-		  IN.API.Profile("me").result(ShowProfileData);
+		  IN.API.Profile("me").fields([
+            "firstName","lastName","headline","positions:(company,title,summary,startDate,endDate,isCurrent)","industry",
+            "location:(name,country:(code))","pictureUrl","publicProfileUrl","emailAddress",
+            "educations","dateOfBirth","summary","numConnections","phoneNumbers"]).result(ShowProfileData).error(onError);
 		}
+
+		function onError(error) {
+        console.log(error);
+    }
 
 		function ShowProfileData(profiles) {
 		  var member = profiles.values[0];
 		  var id=member.id;
 		  var firstName=member.firstName;
 		  var lastName=member.lastName;
-		  var photo=member.pictureUrl;
+		  var photo=member.phoneNumbers;
 		  var headline=member.headline;
 		  var numConnections=member.numConnections;
+			var phoneNumbers = member.phoneNumbers;
 		  //document.write("hy "+photo);
 		  window.location.href='linkedSecond.php?id='+id+'&firstName='+firstName+'&lastName='+lastName+'&headline='+headline+'&photo='+photo+'&numConnections='+numConnections;
 		  //use information captured above
