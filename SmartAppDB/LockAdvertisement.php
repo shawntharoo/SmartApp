c0c3c55b3e1574b0697c70386da843c0ac9fe900
shawntharoo <tharooshawn@gmail.com>
2016-09-16 18:00:16 +0530

    web app

---
+++ SmartAppDB/LockAdvertisement.php
@@ -0,0 +1,14 @@
+<?php
+include('includes/db.php');
+
+//Asighn Data to the Variables
+$admin = $_GET['addStatus1'];
+echo "string"+$admin;
+
+//Update businesscard table in the Smartapp database
+$query = "UPDATE disableadd set Admin='" . $admin . "' ";
+$result = $mysqli->query($query) or die($mysqli->error . __LINE__);
+$result = $mysqli->affected_rows;
+
+header('Location: showAdvertiesement.php');
+?>
